8ee37930025f0c124b3a398d4c139d03deac2c81
Peter Alexander Garnæs <petergarnaes@gmail.com>
2017-12-12 18:55:25 +0100

    Output and generator includes comments on posts

--- generator.js
+++ generator.js
@@ -2,8 +2,18 @@ let fs = require('fs');
 let path = require('path');
 let Text = require('markov-chains-text').default;
 
-const penhouse = ""+fs.readFileSync('./output');
+const penhouse = ""+fs.readFileSync('./output.json');
 //console.log(""+penhouse);
+
+let messages = JSON.parse(penhouse);
+
+let document = messages.map((o)=>{
+    let post = o.message;
+    let comments = (o.comments && o.comments.data) ? o.comments.data : [];
+    let commentMessages = comments.map(c=>c.message);
+    let commentString = commentMessages.join('\n');
+    return post+'\n'+commentString;
+}).join('\n');
 
 const fakePenthouse = new Text(penhouse);
 
